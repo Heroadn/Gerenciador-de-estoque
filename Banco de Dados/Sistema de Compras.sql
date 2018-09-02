0907f231726e2d84f998e9aa3b37761ebd3b6b8f
Heroadn <36571620+Heroadn@users.noreply.github.com>
2018-09-02 15:00:48 -0300

    Add files via upload
    SQL Banco de Dados

--- Banco de Dados/Sistema de Compras.sql
+++ Banco de Dados/Sistema de Compras.sql
@@ -1,24 +1,45 @@
+##HEROADN 02/09/2018##
 CREATE SCHEMA sistema;
 USE sistema;
 
+####################
+#TABELA DE CLIENTES#
+####################
 create table `pessoas`(
-	cod integer primary key auto_increment,
-    nome varchar(30),
-    idade integer
+	cod integer primary key auto_increment not null,
+    nome varchar(30) not null,
+    idade integer not null
 );
 
+####################
+#TABELA DE PRODUTOS#
+####################
 create table `coisas`(
 	cod integer primary key auto_increment,
-    nome varchar(30),
-    valor integer
+    nome varchar(30) not null,
+    valor integer not null
 );
 
+###################
+#TABELA DE COMPRAS#
+###################
+create table pessoas_coisas(
+	cod integer primary key auto_increment not null,
+    id_pessoas integer not null,
+    id_coisas integer not null,
+    FOREIGN KEY (id_pessoas) REFERENCES pessoas(cod),
+    FOREIGN KEY (id_coisas)  REFERENCES coisas(cod)
+);
+
+###########################
+#TABELA DE ADMINISTRADORES#
+###########################
 create table `admin`(
 	id integer primary key auto_increment,
     nome varchar(20) not null,
     senha varchar(32) not null
 );
 
-SELECT * from pessoas;
-
+#ADMIN DE TESTE
 INSERT INTO `admin`(nome,senha) VALUES("default","123");
+SELECT * from `admin`;
